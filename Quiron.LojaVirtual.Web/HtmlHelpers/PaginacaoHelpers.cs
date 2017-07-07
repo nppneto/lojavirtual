4119f5db553653e4f140e29e91423d5925526a36
Nelson <nelsonp2n@gmail.com>
2017-07-07 01:20:08 -0300

    Classes de Paginação

---
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,38 @@
+﻿using Quiron.LojaVirtual.Web.Models;
+using System;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Quiron.LojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        // total de paginas = 3
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1; i < paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+
+                // adiciona href como atributo
+                tag.MergeAttribute("href",paginaUrl(i));
+                // id da página
+                tag.InnerHtml = i.ToString();
+
+                // adiciona o css para definir cor do seletor de pagina quando
+                // na atual
+                if (i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Models/Paginacao.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Quiron.LojaVirtual.Web.Models
+{
+    public class Paginacao
+    {
+        // quantos itens tenho no banco
+        public int ItensTotal { get; set; }
+
+        // quantos itens quero por página
+        public int ItensPorPagina { get; set; }
+
+        // cor da página atual na paginação
+        public int PaginaAtual { get; set; }
+
+        // quantas paginas vou ter
+        public int TotalPagina
+        {
+            get
+            {   // a quantidade de quadrados na paginação
+                return (int)Math.Ceiling((decimal)ItensTotal / ItensPorPagina);
+            } 
+        }
+    }
+}
