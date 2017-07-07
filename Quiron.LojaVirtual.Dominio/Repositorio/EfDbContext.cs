3786f2cb0ae5707757100ba848d1e9c06b337652
Nelson <nelsonp2n@gmail.com>
2017-07-06 23:09:14 -0300

    Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    // dbcontext = classe onde vou colocar todos os meus dbsets
+    public class EfDbContext : DbContext
+    {
+        // dbset =
+        public DbSet<Produto> Produtos { get; set; }
+
+        // remover pluralidade no banco (ex: pessoa - pessoas)
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // using System.Data.Entity.ModelConfiguration.Conventions;
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            // classe produto no código = tabela produtos no banco
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Dominio.Repositorio;
+
+namespace Quiron.LojaVirtual.Web.Controllers
+{
+    public class ProdutosController : Controller
+    {
+
+        private ProdutosRepositorio _repositorio;
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            // instancia do objeto ProdutosRepositorio
+            _repositorio = new ProdutosRepositorio();
+            // tudo o que for obtido no banco através do
+            // _repositorio.Produtos será atribuído a variavel
+            // produtos
+            var produtos =_repositorio.Produtos.Take(3);
+
+
+            // e será passado como parâmetro para a view
+            return View(produtos);
+        }
+    }
+}
